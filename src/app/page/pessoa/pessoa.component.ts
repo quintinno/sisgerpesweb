a59b69f849e7f87b893d3d23b4bdbb16cbdc209a
Desenvolvimento - Quintino Digital <desenvolvimento@quintino.digital>
2023-01-01 18:21:51 -0300

    :dart: Feature: Implementar Paginacao de Dados

--- src/app/page/pessoa/pessoa.component.ts
+++ src/app/page/pessoa/pessoa.component.ts
@@ -10,19 +10,52 @@ export class PessoaComponent implements OnInit {
 
   public pessoaList: any[] = [];
 
+  public pessoaPaginationList: any[] = [];
+
+  public quantidadePorPagina: number = 6;
+
+  public selectedPage = 1;
+
   constructor (
     private pessoaService: PessoaService
   ) { }
 
-  ngOnInit() { 
+  ngOnInit() {
     this.recuperarPessoa();
+    this.configurarPaginacao();
+  }
+
+  private configurarPaginacao() {
+    let pageIndex = (this.selectedPage - 1) * this.quantidadePorPagina;
+    this.pessoaPaginationList = this.pessoaList.slice(pageIndex, this.quantidadePorPagina);
   }
 
   public recuperarPessoa() {
     return this.pessoaService.recuperarPessoa().subscribe( response => {
       this.pessoaList = response;
-      console.log(this.pessoaList);
     });
+  }
+
+  public changePagina(event: Event) {
+    const newSize = (event.target as HTMLInputElement).value;
+    this.quantidadePorPagina = Number(newSize);
+    this.mudarPagina(1);
+  }
+
+  public get pageNumber() : number[] {
+    return Array(Math.ceil(this.pessoaList.length / this.quantidadePorPagina)).fill(0).map( (x, i) => i + 1 );
+  }
+
+  public mudarPagina(page: any) { 
+    this.selectedPage = page;
+    this.sliceList();
+  }
+
+  public sliceList() {
+    let pageIndex = (this.selectedPage - 1) * this.quantidadePorPagina;
+    let endIndex = (this.selectedPage - 1) * this.quantidadePorPagina + this.quantidadePorPagina;
+    this.pessoaPaginationList = [];
+    this.pessoaPaginationList = this.pessoaList.slice(pageIndex, endIndex);
   }
 
 }
